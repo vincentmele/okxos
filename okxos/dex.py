53ba87cc7234c021b53e64e8c715c25418dd38eb
Vincent Mele <vince@mele.law>
2025-01-06 20:23:32 +0100

    SWAP API support (everything but swap)

--- okxos/dex.py
+++ okxos/dex.py
@@ -3,42 +3,51 @@ from okxos.client import OKXClient
 class DEXAPI:
     def __init__(self, client: OKXClient):
         self.client = client
-    #@classmethod
-    #async def create(cls, client: OKXClient):
-    #    self = cls()
-    #    self.client = client
-    #    self.chain_id = chain_id
-    #    self.pool = await self._get_tokens(self.chain_id)
-    #    return self
-    
-    #async def _get_tokens(self, chain_id):
-    #    endpoint = "/api/v5/dex/aggregator/all-tokens"
-    #    params = {"chainId": chain_id}
-    #    raw = await self.client.request("GET", endpoint, params=params)
-    #    if raw:
-    #        tokens = {}
-    #        for t in raw['data']:
-    #            tokens[t['tokenSymbol']] = t
-    #        self.tokens = tokens
 
-    async def get_supported_chains(self, chain_id=None):
+    async def get_supported_chains(self, chain_id=1):
+        # https://www.okx.com/web3/build/docs/waas/dex-get-aggregator-supported-chains
         endpoint = "/api/v5/dex/aggregator/supported/chain"
         params = {"chainId": chain_id} if chain_id else None
         return await self.client.request("GET", endpoint, params=params)
 
-    # 获取币种列表
-    async def get_all_tokens(self, chain_id):
+    async def get_all_tokens(self, chain_id=1):
+        # https://www.okx.com/web3/build/docs/waas/dex-get-tokens
         endpoint = "/api/v5/dex/aggregator/all-tokens"
         params = {"chainId": chain_id}
         return await self.client.request("GET", endpoint, params=params)
     
-    async def get_quote(self, chain_id, token_in, token_out, amount_in):
+    async def get_liquidity_sources(self, chain_id=1):
+        # https://www.okx.com/web3/build/docs/waas/dex-get-tokens
+        endpoint = "/api/v5/dex/aggregator/all-tokens"
+        params = {"chainId": chain_id}
+        return await self.client.request("GET", endpoint, params=params)
+    
+    async def approve_transactions(self, token_contract_address: str, approve_amt: float, chain_id=1):
+        # https://www.okx.com/web3/build/docs/waas/dex-get-tokens
+        endpoint = "/api/v5/dex/aggregator/all-tokens"
+        params = {"chainId": chain_id,
+                  "tokenContractAddress": token_contract_address,
+                  "approveAmount": approve_amt}
+        return await self.client.request("GET", endpoint, params=params)
+
+    async def get_quote(self, amount_in: float, from_token_address, 
+                        to_token_address, dex_ids=None, price_impact_protection_percentage=None, 
+                        fee_percent=None, chain_id=1):
+        # https://www.okx.com/web3/build/docs/waas/dex-get-quote
         endpoint = "/api/v5/dex/aggregator/quote"
         params = {
             "chainId": chain_id,
-            "fromTokenAddress": token_in,
-            "toTokenAddress": token_out,
+            "fromTokenAddress": from_token_address,
+            "toTokenAddress": to_token_address,
             "amount": amount_in,
+            "dexIds": dex_ids,
+            "priceImpactProtectionPercentage": price_impact_protection_percentage,
+            "feePercent": fee_percent
         }
         return await self.client.request("GET", endpoint, params=params)
     
+    async def swap(self,):
+        # https://www.okx.com/web3/build/docs/waas/dex-swap
+        raise NotImplemented()
+    
+    # TODO: Implement error codes: https://www.okx.com/web3/build/docs/waas/dex-error-code
